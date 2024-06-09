a7d2635f03ad938f1548a122b86318669881ec91
“onahfran6” <fonah423@stu.ui.edu.ng>
2024-06-10 00:25:10 +0100

    update the health endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,39 @@
-import express, { Request, Response, NextFunction } from "express";
-import compression from "compression";
-import bodyParser from "body-parser";
-import lusca from "lusca";
-import path from "path";
-import cors from "cors";
-import helmet from "helmet";
-import errorHandler from "errorhandler";
+import express, { Request, Response, NextFunction } from 'express';
+import compression from 'compression';
+import bodyParser from 'body-parser';
+import lusca from 'lusca';
+import path from 'path';
+import cors from 'cors';
+import helmet from 'helmet';
+import errorHandler from 'errorhandler';
 
-import config from "./config";
+import config from './config';
 import {
   errorHandler as customErrorHandler,
   notFoundHandler,
-} from "./middleware/errorMiddleware";
-
-
+} from './middleware/errorMiddleware';
 
 // Create Express server
 const app = express();
 
 // Express configuration
-app.set("port", config.PORT );
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "pug");
+app.set('port', config.PORT);
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
 app.use(compression());
 app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(lusca.xframe("SAMEORIGIN"));
+app.use(lusca.xframe('SAMEORIGIN'));
 app.use(lusca.xssProtection(true));
 
 app.use(
-  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+  express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }),
 );
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Server is healthy");
+app.get('/apis/v1/', (req: Request, res: Response) => {
+  res.send('Server is healthy');
 });
 
 // Error handling middleware
@@ -43,7 +41,7 @@ app.use(notFoundHandler);
 app.use(customErrorHandler);
 
 // Development error handler
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   app.use(errorHandler());
 }
 
